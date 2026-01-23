a479726c1a9c9936a823b34500d0ab115eecb928
Pavel Vazharov <freakpv@gmail.com>
2026-01-23 17:09:34 +0200

    Remove the need of a flag for the vyukov_mpmc_queue elements

--- lib/par/include/par/vyukov_mpmc_queue.hpp
+++ lib/par/include/par/vyukov_mpmc_queue.hpp
@@ -12,18 +12,9 @@ namespace freak::par
 template <typename T>
 class vyukov_mpmc_queue
 {
-    // The position/sequence need to be limited to 63 bits due to the need of
-    // one bit for a flag if the corresponding `data_` has been constructed
-    // or not.
-    static constexpr uint64_t seq_mask = (1ull << 63) - 1;
     struct cell
     {
-        struct flag_seq
-        {
-            uint64_t flag_ : 1;
-            uint64_t seq_  : 63;
-        };
-        std::atomic<flag_seq> flag_seq_;
+        std::atomic<uint64_t> seq_;
 
         union u
         {
@@ -52,208 +43,111 @@ public:
     vyukov_mpmc_queue& operator=(vyukov_mpmc_queue&&)      = delete;
     vyukov_mpmc_queue& operator=(const vyukov_mpmc_queue&) = delete;
 
-    // The given size must be greater than 1 and smaller than 2^63 and
-    // power of 2
+    // The given size must be greater than 1 and power of 2
     explicit vyukov_mpmc_queue(size_t size)
     : cells_(std::make_unique<cell[]>(size)), pos_mask_(size - 1)
     {
-        assert((size >= 2) && (size <= seq_mask) && ((size & (size - 1)) == 0));
+        assert((size >= 2) && ((size & (size - 1)) == 0));
 
-        static_assert(decltype(cell::flag_seq_)::is_always_lock_free);
+        static_assert(decltype(cell::seq_)::is_always_lock_free);
         static_assert(decltype(push_pos_)::is_always_lock_free);
         static_assert(decltype(pop_pos_)::is_always_lock_free);
 
         for (size_t i = 0; i < size; ++i) {
-            cells_[i].flag_seq_.store(
-                {
-                    .flag_ = false,
-                    .seq_  = i & seq_mask,
-                },
-                std::memory_order_relaxed);
+            cells_[i].seq_.store(i, std::memory_order_relaxed);
         }
     }
 
     ~vyukov_mpmc_queue() noexcept
     {
         if constexpr (!std::is_trivially_destructible_v<value_type>) {
-            const auto size = pos_mask_ + 1;
-            for (auto& c : std::span(cells_.get(), size)) {
-                const auto fc = c.flag_seq_.load(std::memory_order_relaxed);
-                if (fc.flag_ == true) [[likely]] { c.u_.data_.~value_type(); }
+            const auto push_pos = push_pos_.load(std::memory_order_relaxed);
+            auto pop_pos        = pop_pos_.load(std::memory_order_relaxed);
+            for (; pop_pos != push_pos; ++pop_pos) {
+                auto* pc = &cells_[pop_pos & pos_mask_];
+                assert(pc->seq_.load(std::memory_order_acquire) == pop_pos + 1);
+                pc->u_.data_.~value_type();
             }
         }
     }
 
     // clang-format off
     template <typename... Args>
-    bool try_push(Args&&... args) 
-        noexcept(std::is_nothrow_constructible_v<T, Args...>)
-        requires(std::is_constructible_v<T, Args...>)
+    bool try_push(Args&&... args) noexcept
+        requires(std::is_nothrow_constructible_v<T, Args...>)
     // clang-format on
     {
-        // cell* pc = nullptr;
-        // const auto pos = push_pos_.load(std::memory_order_relaxed);
-        // for (;;) {
+        cell* pc     = nullptr;
+        uint64_t pos = push_pos_.load(std::memory_order_relaxed);
 
-        //}
+        for (;;) {
+            pc = &cells_[pos & pos_mask_];
+
+            const auto seq = pc->seq_.load(std::memory_order_acquire);
+            const auto dif =
+                static_cast<int64_t>(seq) - static_cast<int64_t>(pos);
+            if (dif == 0) {
+                if (push_pos_.compare_exchange_weak(
+                        pos, pos + 1, std::memory_order_relaxed)) {
+                    break;
+                }
+            } else if (dif > 0) {
+                pos = push_pos_.load(std::memory_order_relaxed);
+            } else {
+                // The queue is full
+                return false;
+            }
+        }
+
+        new (&pc->u_.data_) value_type(std::forward<Args>(args)...);
+        pc->seq_.store(pos + 1, std::memory_order_release);
+
+        return true;
+    }
+
+    // clang-format off
+    template <typename U>
+    bool try_pop(U& to) noexcept
+        requires(std::is_nothrow_assignable_v<U&, T&&>)
+    // clang-format on
+    {
+        return try_pop_fn([&to](T&& v) noexcept { to = std::move(v); });
+    }
+
+    // clang-format off
+    template <typename Fn>
+    bool try_pop_fn(Fn&& fn) noexcept
+        requires(std::is_nothrow_invocable_v<Fn, value_type&&>)
+    // clang-format on
+    {
+        cell* pc     = nullptr;
+        uint64_t pos = pop_pos_.load(std::memory_order_relaxed);
+
+        for (;;) {
+            pc = &cells_[pos & pos_mask_];
+
+            const auto seq = pc->seq_.load(std::memory_order_acquire);
+            const auto dif =
+                static_cast<int64_t>(seq) - static_cast<int64_t>(pos + 1);
+            if (dif == 0) {
+                if (pop_pos_.compare_exchange_weak(pos, pos + 1,
+                                                   std::memory_order_relaxed)) {
+                    break;
+                }
+            } else if (dif > 0) {
+                pos = pop_pos_.load(std::memory_order_relaxed);
+            } else {
+                // The queue is empty
+                return false;
+            }
+        }
+
+        std::forward<Fn>(fn)(std::move(pc->u_.data_));
+        pc->u_.data_.~value_type();
+        pc->seq_.store(pos + 1 + pos_mask_, std::memory_order_release);
 
         return true;
     }
 };
-
-/*
-template <typename T>
-class mpmc_bounded_queue
-
-{
-public:
-    mpmc_bounded_queue(size_t buffer_size)
-
-    : buffer_(new cell_t[buffer_size])
-
-    , buffer_mask_(buffer_size - 1)
-
-    {
-        assert((buffer_size >= 2) &&
-
-               ((buffer_size & (buffer_size - 1)) == 0));
-
-        for (size_t i = 0; i != buffer_size; i += 1)
-
-            buffer_[i].sequence_.store(i, std::memory_order_relaxed);
-
-        enqueue_pos_.store(0, std::memory_order_relaxed);
-
-        dequeue_pos_.store(0, std::memory_order_relaxed);
-    }
-
-    ~mpmc_bounded_queue() { delete[] buffer_; }
-
-    bool enqueue(T const& data)
-
-    {
-        cell_t* cell;
-
-        size_t pos = enqueue_pos_.load(std::memory_order_relaxed);
-
-        for (;;)
-
-        {
-            cell = &buffer_[pos & buffer_mask_];
-
-            size_t seq =
-
-                cell->sequence_.load(std::memory_order_acquire);
-
-            intptr_t dif = (intptr_t)seq - (intptr_t)pos;
-
-            if (dif == 0)
-
-            {
-                if (enqueue_pos_.compare_exchange_weak
-
-                    (pos, pos + 1, std::memory_order_relaxed))
-
-                    break;
-
-            }
-
-            else if (dif < 0)
-
-                return false;
-
-            else
-
-                pos = enqueue_pos_.load(std::memory_order_relaxed);
-        }
-
-        cell->data_ = data;
-
-        cell->sequence_.store(pos + 1, std::memory_order_release);
-
-        return true;
-    }
-
-    bool dequeue(T& data)
-
-    {
-        cell_t* cell;
-
-        size_t pos = dequeue_pos_.load(std::memory_order_relaxed);
-
-        for (;;)
-
-        {
-            cell = &buffer_[pos & buffer_mask_];
-
-            size_t seq =
-
-                cell->sequence_.load(std::memory_order_acquire);
-
-            intptr_t dif = (intptr_t)seq - (intptr_t)(pos + 1);
-
-            if (dif == 0)
-
-            {
-                if (dequeue_pos_.compare_exchange_weak
-
-                    (pos, pos + 1, std::memory_order_relaxed))
-
-                    break;
-
-            }
-
-            else if (dif < 0)
-
-                return false;
-
-            else
-
-                pos = dequeue_pos_.load(std::memory_order_relaxed);
-        }
-
-        data = cell->data_;
-
-        cell->sequence_.store
-
-            (pos + buffer_mask_ + 1, std::memory_order_release);
-
-        return true;
-    }
-
-private:
-    struct cell_t
-
-    {
-        std::atomic<size_t> sequence_;
-
-        T data_;
-    };
-
-    static size_t const cacheline_size = 64;
-
-    typedef char cacheline_pad_t[cacheline_size];
-
-    cacheline_pad_t pad0_;
-
-    cell_t* const buffer_;
-
-    size_t const buffer_mask_;
-
-    cacheline_pad_t pad1_;
-
-    std::atomic<size_t> enqueue_pos_;
-
-    cacheline_pad_t pad2_;
-
-    std::atomic<size_t> dequeue_pos_;
-
-    cacheline_pad_t pad3_;
-
-    mpmc_bounded_queue(mpmc_bounded_queue const&);
-
-    void operator=(mpmc_bounded_queue const&);
-};
-*/
 
 } // namespace freak::par
--- lib/par/src/vyukov_mpmc_queue.cpp
+++ lib/par/src/vyukov_mpmc_queue.cpp
@@ -5,5 +5,10 @@
 [[maybe_unused]]
 static void verify_compilation()
 {
-    [[maybe_unused]] freak::par::vyukov_mpmc_queue<int> q(2);
+    freak::par::vyukov_mpmc_queue<int> q(2);
+    q.try_push(42);
+    q.try_push(84);
+    int i = 0;
+    q.try_pop(i);
+    q.try_pop_fn([&i](int ii) noexcept { i = ii; });
 }
